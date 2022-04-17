a1db72ec0f0ca6a4eabe43aad240c3c96fa24850
Kirill Panov <kirpa8604@gmail.com>
2022-04-17 11:06:09 -0700

    Added more details in README file

--- Blockchain Ledger with Web on streamlit/pychain.py
+++ Blockchain Ledger with Web on streamlit/pychain.py
@@ -145,33 +145,3 @@ if st.button("Validate Chain"):
 st.header("Block Inspector")
 selected_block = st.selectbox("Which block would you like to see?", pychain.chain)
 st.write(selected_block)
-
-
-
-################################################################################
-# Step 4:
-# Test the PyChain Ledger by Storing Records
-
-# Test your complete `PyChain` ledger and user interface by running your
-# Streamlit application and storing some mined blocks in your `PyChain` ledger.
-# Then test the blockchain validation process by using your `PyChain` ledger.
-# To do so, complete the following steps:
-
-# 1. In the terminal, navigate to the project folder where you've coded the
-#  Challenge.
-
-# 2. In the terminal, run the Streamlit application by
-# using `streamlit run pychain.py`.
-
-# 3. Enter values for the sender, receiver, and amount, and then click the "Add
-# Block" button. Do this several times to store several blocks in the ledger.
-
-# 4. Verify the block contents and hashes in the Streamlit drop-down menu.
-# Take a screenshot of the Streamlit application page, which should detail a
-# blockchain that consists of multiple blocks. Include the screenshot in the
-# `README.md` file for your Challenge repository.
-
-# 5. Test the blockchain validation process by using the web interface.
-# Take a screenshot of the Streamlit application page, which should indicate
-# the validity of the blockchain. Include the screenshot in the `README.md`
-# file for your Challenge repository.
